6377e3d913459d52b00ab7664a207d18b10b21c1
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-08-07 21:57:33 -0300

    listener

--- Turma A/Aula 01/programa.js
+++ Turma A/Aula 01/programa.js
@@ -6,3 +6,10 @@ console.log(`SOMA :: ${z}`);
 function soma(a, b=10){
   return a + b;
 }
+
+var teste = function(){
+  let resultado = soma(10,50);
+  console.log(resultado);
+}
+
+window.addEventListener("click", teste);
