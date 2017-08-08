a23f86a97dc7a3b609d86c783cca95dbbb521659
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-08-07 21:42:23 -0300

    console

--- Turma A/Aula 01/programa.js
+++ Turma A/Aula 01/programa.js
@@ -1,7 +1,7 @@
-var x = 10;
+var x = "10";
 var y = "A dona Aranha subiu pela parede...";
-var z = soma(x,x);
-alert(`SOMA :: ${z}`);
+var z = soma(x);
+console.log(`SOMA :: ${z}`);
 
 function soma(a, b=10){
   return a + b;
