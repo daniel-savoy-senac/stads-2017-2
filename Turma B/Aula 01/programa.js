05b34edc79cd6f799eb993c7f43db9ff012f04d1
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-08-10 21:47:34 -0300

    js

--- Turma B/Aula 01/programa.js
+++ Turma B/Aula 01/programa.js
@@ -1,7 +1,10 @@
 function colorir(evt){
   let x = evt.x;
   let y = evt.y;
-  document.body.style.background = "red";
+  let lar = window.innerWidth;
+  let r = ~~(x / lar * 255);
+  console.log(`rgb(${r},0,0)`);
+  document.body.style.background = `rgb(${r},0,0)`;
 }
 
 window.addEventListener("click", colorir);
