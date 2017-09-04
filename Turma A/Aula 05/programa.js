cef895824a9c907adf9fb13b15cf3e838d375545
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-09-04 20:10:56 -0300

    TESTE

--- Turma A/Aula 05/programa.js
+++ Turma A/Aula 05/programa.js
@@ -1,6 +1,12 @@
 function iniciar() {
   let teste = document.querySelector("#teste");
   console.log(teste);
+  let x = 1 - 2;
+  console.log(x);
+  let texto = true;
+  console.log(texto);
+  let gambi = false == 0;
+  console.log(gambi);
 }
 
 window.addEventListener("load", iniciar);
