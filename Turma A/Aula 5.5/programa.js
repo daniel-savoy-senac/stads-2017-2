4e51a614c23845947115e82d7f04074b915eb4ce
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-09-11 21:33:06 -0300

    criar

--- Turma A/Aula 5.5/programa.js
+++ Turma A/Aula 5.5/programa.js
@@ -7,13 +7,18 @@ let iniciar = () => {
   let elm3 = document.getElementById("raiz");
   let elm4 = document.getElementsByTagName("body");
   let elm5 = document.getElementsByClassName("teste");
+};
 
+let criar = (evt) => {
   // Criar NOVO
   // PARAM: Nome da Tag
   let novo = document.createElement("div");
   novo.setAttribute("class", "novo");
+  novo.style.left = `${evt.x}px`;
+  novo.style.top = `${evt.y}px`;
   console.log(novo);
-
+  document.body.appendChild(novo);
 };
 
 window.addEventListener("load", iniciar);
+window.addEventListener("click", criar);
