8641ba28a501cb4ecab9cb57d5fb16d4bcaa269e
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-10-23 19:21:00 -0200

    aula 08

---
+++ Turma A/Aula 08/GirafaBebada/src/java/Inicio.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Inicio extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        GregorianCalendar cal = new GregorianCalendar();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        
+        String msg = "Vai dormir!!!";
+        
+        if(hour > 6 && hour <= 12) msg = "Boa manhã";
+        else if(hour > 12 && hour <= 15) msg = "Bom dia";
+        else if(hour > 15 && hour <= 18) msg = "Boa tarde";
+        else if(hour > 18 && hour <= 24) msg = "Boa noite";
+        /*
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<body>");
+            out.println("<h1>"+msg+"</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        */
+        request.setAttribute("msg", msg);
+        request.setAttribute("hour", hour);
+        request.getRequestDispatcher("/WEB-INF/main.jspx").forward(request, response);
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<body>");
+            out.println("<h1>A Girafa bebeu toda a tequila!!!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+}
---
+++ Turma A/Aula 08/GirafaBebada/web/WEB-INF/main.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : main
+    Created on : 16/10/2017, 19:50:11
+    Author     : daniel.psavoy
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1><%= msg %></h1>
+    </body>
+</html>
