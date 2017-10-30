2a25bcec1f136a2d0543f49fa9dd36399f87f95a
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-10-30 20:20:20 -0200

    xhr

--- Turma A/Aula 09/XHR/programa.js
+++ Turma A/Aula 09/XHR/programa.js
@@ -1,1 +1,9 @@
-alert("ok");
+let xhr = new XMLHttpRequest();
+xhr.addEventListener("load", atualizar);
+xhr.open("data.json", "get");
+xhr.send();
+
+
+function atualizar(evt){
+  console.log(evt);
+}
