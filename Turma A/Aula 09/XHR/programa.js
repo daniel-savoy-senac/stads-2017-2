126a9d8438cfe74db18e78f481535a02e8dd49a5
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-10-30 22:00:14 -0200

    exemplo

--- Turma A/Aula 09/XHR/programa.js
+++ Turma A/Aula 09/XHR/programa.js
@@ -2,7 +2,22 @@
 
 fetch("data.json")
   .then(r => r.json())
-  .then(j => console.log(j));
+  .then(j => atualizarTela(j));
+
+function atualizarTela(obj){
+  console.log(obj)
+  for(let i = 0; i < obj.msg.length; i++){
+    let usr = obj.usuarios[i % 2];
+    imprimir(usr, obj.msg[i]);
+  }
+}
+
+function imprimir(usr, msg){
+  let p = document.createElement("p");
+  p.textContent = `${usr} disse: ${msg}`;
+  document.body.appendChild(p);
+}
+
 
 /*
 // XHR
