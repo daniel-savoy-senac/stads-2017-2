3a52e0f66027d054f131773491cdeb200b1a720b
DANIEL PRATA SAVOY <daniel.psavoy@senacsp.edu.br>
2017-11-06 21:27:35 -0200

    multipart

--- Turma A/Aula 10/programa.js
+++ Turma A/Aula 10/programa.js
@@ -4,9 +4,24 @@ function main(){
 }
 
 function sendData(){
-  let valor = document.querySelector("input").value;
+  let form = document.querySelector("form");
+  let action = form.getAttribute("action");
+  let data = new FormData(form);
+  fetch(action, {method: "post", body: data})
+    .then(r => r.text())
+    .then(t => console.log(t));
+}
+
+function sendData2(){
+  let inputs = [...document.querySelectorAll("form *[name]")];
+
+  let param = inputs
+    .map(i => `${i.getAttribute("name")}=${i.value}`)
+    .join("&");
+
   let action = document.querySelector("form").getAttribute("action");
-  fetch(`${action}?texto=${valor}`)
+
+  fetch(`${action}?${param}`)
     .then(r => r.text())
     .then(t => console.log(t));
 
